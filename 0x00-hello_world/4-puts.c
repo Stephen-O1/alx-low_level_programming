93062a80b35ed00c7bb84bbc2bbb2ca6c08012cf
Stephen-O1 <onyangostephen654@gmail.com>
2023-08-10 07:42:47 -0700

    Correction for 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*Introduce the main function*/
 int main(void)
+/*The main function will use puts*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
