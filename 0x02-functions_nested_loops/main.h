5926de5845642ce8d991369e535fa93ab736786d
Stephen-O1 <onyangostephen654@gmail.com>
2023-08-15 06:58:12 -0700

    Printing sign of a number

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,6 +30,15 @@ void print_alphabet_x10(void);
  */
 
 int _islower(int c);
+
+/**
+ * print_sign - print sign of number
+ * @n: number to check
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+ */
+
+int print_sign(int n);
+
 /**
  * print_last_digit - print last digit of integer
  * @int: integer
