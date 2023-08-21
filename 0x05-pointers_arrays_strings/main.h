73e8a39776622232fbe83137b668231fb8fa7b19
Stephen-O1 <onyangostephen654@gmail.com>
2023-08-21 06:12:41 -0700

    Preseting integer value

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * @n: pointer to int n
+ */
+
+void reset_to_98(int *n);
