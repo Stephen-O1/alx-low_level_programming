1fc9f246f1eb2979c3416df703e6b40b61dc2800
Stephen-O1 <onyangostephen654@gmail.com>
2023-08-21 09:31:20 -0700

    Program that prints random character

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,15 +3,21 @@
 #include <time.h>
 
 #define PASSWORD_LENGTH 10
+
 /**
- *Writing a program that generates password randomly
+ *Writing a program that print random passwords
+ *Defining the random paasword function
  */
+
 void generateRandomPassword(char *password, int length)
 {
 const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 size_t charset_length = sizeof(charset) - 1;
 
-for (int i = 0; i < length; i++)
+int i;
+/* Move the declaration outside the loop*/
+ 
+for (i = 0; i < length; i++)
 {
 password[i] = charset[rand() % charset_length];
 }
@@ -21,10 +27,13 @@ password[length] = '\0';
 int main()
 {
 srand(time(NULL));
+
 int passwordLength = PASSWORD_LENGTH;
 char password[PASSWORD_LENGTH + 1];
+
 generateRandomPassword(password, passwordLength);
+
 printf("Generated Password: %s\n", password);
-    
+
 return 0;
 }
